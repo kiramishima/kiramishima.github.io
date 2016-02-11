752a849deebc854b5efff29e28c8bf4cf90af03a
Paul Arizpe <paul.arizpe@arteika>
2016-02-10 19:57:02 -0600

    Update

--- src/Components/skills.jsx
+++ src/Components/skills.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import '../../sass/skills.scss'
+import {Knowledges} from './knowledges.jsx'
 
 export class Skill extends Component {
 	skill () {
-		console.log(this)
+		// console.log(this)
+		ReactDOM.render(<Knowledges key={this.props.name} lang={this.props.name} skills={this.props.knowledge} />, document.getElementById('knowledgeContent'))
 	}
 	render () {
-		return <div className="chip" onClick={this.skill.bind(this)}><img src={this.props.src} alt="Contact Person"/> {this.props.name}</div>
+		return <div className="chip" onClick={this.skill.bind(this)}><img src={this.props.src} alt="Contact Person"/> {this.props.name} </div>
 	}
 }
 
@@ -17,16 +21,14 @@ export default class Skills extends Component {
 
 	componentDidMount() {
 		(async () => {
-			console.log(1222)
 			const response = await fetch('./build/skills.json')
 			const json = await response.json()
-			console.log(json)
 			this.setState(json)
 		})()
 	}
 
 	render () {
-		var items = this.state.skills.map((item) => <Skill key={item.name}  name={item.name} src={item.logo} />)
+		var items = this.state.skills.map((item) => <Skill key={item.name}  name={item.name} src={item.logo} knowledge={item.knowledge} />  )
 		return (
 			<div className="skills">
 				<h3>Skills</h3>
--- src/app.js
+++ src/app.js
@@ -11,16 +11,22 @@ class Application extends React.Component {
 		super(props)
 	}
 	render () {
-		return (<div className="row">
-			<div className="col s6">
-				<Avatar source="https://api.github.com/users/kiramishima" />
+		return (<div>
+			<div className="row">
+				<div className="col col-33">
+					<Avatar source="https://api.github.com/users/kiramishima" />
+				</div>
+				<div className="col">
+					<h5>Paul Emilio Arizpe Colorado</h5>
+					<h8>TSU Tecnologias de la información y comunicación</h8>
+				</div>
 			</div>
-			<div className="col s6 m6">
-				<h5>Paul Emilio Arizpe Colorado</h5>
+			<div className="row">
+				<div className="col">
+					<Skills />
+				</div>
 			</div>
-
-			<div className="col s12 m12">
-				<Skills />
+			<div className="row" id="knowledgeContent">
 			</div>
 		</div>)
 	}
